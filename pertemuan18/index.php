68f5c41a1403da6b6ef5f2f56f33eddf6735e252
jaenudin0901 <jaenudinj913198@gmail.com>
2021-03-14 01:54:57 +0700

    menambahkan folder pertemuan18 membuat pagination

--- pertemuan18/index.php
+++ pertemuan18/index.php
@@ -8,8 +8,31 @@ if (!isset($_SESSION["login"])) {
 
 }
  
+
+// paganation
+// konfirmasi
+
+$jumlahDataPerHalaman = 2;
+
+
+$jumlahData = count(query("SELECT * FROM mahasiswa"));
+$jumlahHalaman =ceil($jumlahData / $jumlahDataPerHalaman);
+
+$halamanAktif = (isset($_GET["halaman"])) ? $_GET["halaman"]:1;
+
+$awalData =( $jumlahDataPerHalaman * $halamanAktif) - $jumlahDataPerHalaman;
+
+$mahasiswa = query("SELECT * FROM mahasiswa LIMIT $awalData, $jumlahDataPerHalaman");
+// if(isset($_GET["halaman"])) {
+// 	$halamanAktif = $_GET["halaman"];
+// } else {
+// 	$halamanAktif = 1;
+// }
+
+
+
 // Ambil data dari tabel mahasiwa /query data mahasiswa
-$mahasiswa = query( "SELECT * FROM mahasiswa ORDER BY id DESC");
+// $mahasiswa = query( "SELECT * FROM mahasiswa LIMIT 0, $jumlahDataPerHalaman");
 
 // tombol cari ditekan
 if(isset($_POST["cari"])){
@@ -40,6 +63,29 @@ if(isset($_POST["cari"])){
 </form>
 
 <br><br>
+
+<!-- navigasi -->
+<?php if($halamanAktif > 1)  : ?>
+<a href="?halaman=<?=$halamanAktif -1; ?>">&laquo;</a>
+<?php endif; ?>
+
+<?php for ($i=1; $i <= $jumlahHalaman ; $i++) :?> 
+
+	<?php if($i == $halamanAktif): ?>
+	<a href="?halaman=<?= $i; ?>" style="font-weight: bold; color: red;" ><?= $i; ?></a>
+
+	<?php else : ?>
+	<a href="?halaman=<?= $i; ?>" ><?= $i; ?></a>
+
+	<?php endif; ?>
+<?php endfor; ?>
+
+<?php if($halamanAktif < $jumlahHalaman)  : ?>
+<a href="?halaman=<?=$halamanAktif + 1; ?>">&raquo</a>
+<?php endif; ?>
+
+
+
 <table border="1" cellpadding="10" cellspacing="0">
 	<tr>
 		<th>No.</th>
